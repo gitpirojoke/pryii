08668bf0f79af292330c9254a693346b17ec1799
korsar <pavel.isupov@mail.ru>
2019-12-19 11:12:38 +0300

    исправлен конфиг дб

--- config/db.php
+++ config/db.php
@@ -1,13 +1,9 @@
 <?php
+
 return [
-    'components' => [
-        // ...
-        'db' => [
-            'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=127.0.0.1;dbname=base1',
-            'username' => 'default',
-            'password' => 'secret',
-            'charset' => 'utf8',
-        ],
-    ],
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=mysql;dbname=base1',
+    'username' => 'root',
+    'password' => 'root',
+    'charset' => 'utf8',
 ];
